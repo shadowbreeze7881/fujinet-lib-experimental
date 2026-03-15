10c47cc899bacfc10c95296fcf335b7e726fb1d2
shadowbreeze7881 <theretrolife@soulexi.com>
2026-03-14 23:13:34 -0400

    starting all over again

--- code/compilers/cc65/base/conio/fc_gotoy.c
+++
@@ -1,7 +0,0 @@
-#include <conio.h>
-#include "fujinet-compiler.h"
-
-void fc_gotox(unsigned char y)
-{
-    gotoy(y);
-}
--- code/compilers/z88dk/base/conio/fc_cputsxy.c
+++
@@ -1,7 +0,0 @@
-#include <conio.h>
-#include "fujinet-compiler.h"
-
-void fc_cputsxy(unsigned char x, unsigned char y, const char *message) {
-    gotoxy(x,y);
-    cputs(message);
-}
